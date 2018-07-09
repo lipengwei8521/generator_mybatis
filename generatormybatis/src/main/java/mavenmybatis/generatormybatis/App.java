2252ddbae522fcdfdd174c551f6de5be8f313198
lipw <903870984@qq.com>
2018-07-09 10:09:49 +0800

    mybatis逆向工程
    mybatis实现的逆向工程

---
+++ generatormybatis/src/main/java/mavenmybatis/generatormybatis/App.java
@@ -0,0 +1,13 @@
+package mavenmybatis.generatormybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
